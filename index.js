51ba6a72ed449f7399514f7f02997e15c3ebb9b3
Marius Nicolae <Marius Nicolae>
2020-04-28 21:27:33 +0000

    Finish working palindrome code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -9,7 +11,11 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  this.letters = function letters() {
+    return this.content.replace(/[^a-zA-Z]/g, '');
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
@@ -18,21 +24,6 @@ function Phrase(content) {
   }
 }
 
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.translation.toLowerCase();
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
-
-let greeting = new Phrase("MadamImAdam");
-let frase = new TranslatedPhrase("recognize", "reconocer");
-console.log(greeting.content);
-console.log(greeting.palindrome());
-console.log(frase.palindrome());
+//let greeting = new Phrase("MadamImAdam");
+//console.log(greeting.content);
+//console.log(greeting.palindrome());
---
+++ test/test.js
@@ -0,0 +1,38 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+
+  describe("#palindrome", function() {
+
+    it("should return false for a non-palindrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
+    });
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let p = new Phrase("MadamImAdam");
+      assert(p.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let p = new Phrase("Madam, in Eden I'm Adam");
+      assert(p.palindrome());
+    });
+  });
+
+  describe("#letters", function() {
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual("MadamImAdam", punctuatedPalindrome.letters());
+
+      punctuatedPalindrome = new Phrase("Madam, in Eden I'm Adam");
+      assert.strictEqual("MadaminEdenImAdam", punctuatedPalindrome.letters());
+    });
+  });
+});
